c4b669f0b93711de2484bf36a56543f3205cb759
Utkarsh Tiwari <utkarshtiwari0860@gmail.com>
2024-10-14 10:25:21 +0530

    added

---
+++ Typescript learning/intro.ts
@@ -0,0 +1,20 @@
+
+let user = {name : "Utkarsh", age : "22"};
+console.log(user.name);
+/*for install typescript 
+Use git bash as administrator in windows
+
+the type command : npm install -g typescript
+
+tsc -v  -> for getting the version of typescript installed in out system
+
+ctrl + ` => for opening terminal in vs code 
+for compiling the typescript file 
+you need to open terminal in vscode and type : tsc filename.ts or filename.tsx
+
+[note in terminal u need to select bash instead of powershell 
+from dropdown show in terminall on tip right side ]
+
+this will generate one javascript file which u can furtther compile as per your use 
+
+*/
---
+++ backend/Java-Basics/src/main/java/org/practice/entity/CustomerDetails.java
@@ -0,0 +1,21 @@
+package org.practice.entity;
+import java.util.Date;
+
+public class CustomerDetails {
+    String name;
+    Date dob;
+    String password;
+    String username;
+    Long money;
+
+    public void CustomerDetails(String name,Date dob,String password,String username,Long money){
+        this.name=name;
+        this.dob=dob;
+        this.password=password;
+        this.username=username;
+        this.money=money;
+    }
+    //https://www.youtube.com/watch?v=KRwbeo1Wn2w&list=PLrlbnvtRPssvjCRgW6eGyZeYkJKPx53FV&index=2
+ Hi, your Zuno Health Policy No. 402000102164 is up for renewal. It's early days still, but it's a wise decision to renew now and stay stress-free.To renew your policy, click http://nc3.in/dBp6KNr Anytime you need us, simply call us on our toll-free number 1800 12000 or landline 022 42312000 (call charges applicable) or write to us at support@hizuno.com. Team Zuno (Formerly known as Edelweiss GI)
+             Hi, it means a lot that you trusted us to cover you. We hope you never need to be hospitalized, but if you do, we're there for you with 100% cashless treatment, anytime, anywhere. Just ask us for pre-authorization 48 hours before you plan to get admitted. If it's an emergency, you can do this up to 48 hours after admission. For more details, please visit our website www.hizuno.com. Anytime you need us, simply call us on our toll-free number 1800 12000 or email us at support@hizuno.com. Team Zuno (Formerly known as Edelweiss GI)
+}
